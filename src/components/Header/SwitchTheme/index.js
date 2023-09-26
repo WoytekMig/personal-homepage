065a987413dce55a85becdefe01ca673593ba102
Wojciech Migdał <122496250+WoytekMig@users.noreply.github.com>
2023-09-26 16:54:44 +0200

    resolved non-boolean attribute in 'slide'

--- src/components/Header/SwitchTheme/index.js
+++ src/components/Header/SwitchTheme/index.js
@@ -1,23 +1,29 @@
-import { ToggleBackground, Graphic, GraphicBackground, Wrapper, StyledButton, ToggleWrapper } from "./styled";
+import {
+  ToggleBackground,
+  Graphic,
+  GraphicBackground,
+  Wrapper,
+  StyledButton,
+  ToggleWrapper,
+} from "./styled";
 import { useDispatch, useSelector } from "react-redux";
 import { selectIsDarkTheme, toggleTheme } from "./themeSlice";
 
 export const ThemeButton = () => {
+  const dispatch = useDispatch();
+  const theme = useSelector(selectIsDarkTheme);
+  console.log("theme colorButton: ", theme);
 
-    const dispatch = useDispatch();
-    const theme = useSelector(selectIsDarkTheme);
-    console.log("theme colorButton: ", theme);
-
-    return (
-        <Wrapper >
-            <StyledButton onClick={() => dispatch(toggleTheme())}>
-                Dark mode {theme ? "on" : "off"}
-                <ToggleWrapper >
-                    <ToggleBackground />
-                    <GraphicBackground slide={theme} />
-                    <Graphic slide={theme} />
-                </ToggleWrapper>
-            </StyledButton>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <StyledButton onClick={() => dispatch(toggleTheme())}>
+        Dark mode {theme ? "on" : "off"}
+        <ToggleWrapper>
+          <ToggleBackground />
+          <GraphicBackground slide={theme ? 1 : 0} />
+          <Graphic slide={theme ? 1 : 0} />
+        </ToggleWrapper>
+      </StyledButton>
+    </Wrapper>
+  );
 };
--- src/components/Skills/styled.js
+++ src/components/Skills/styled.js
@@ -1,18 +1,18 @@
 import { styled } from "styled-components";
 
 export const Wrapper = styled.div`
-    max-width: 1216px;
-    background-color: ${({ theme }) => theme.skillsBackground};
-    padding: 32px;
+  max-width: 1216px;
+  background-color: ${({ theme }) => theme.skillsBackground};
+  padding: 32px;
 `;
 
 export const Header = styled.header`
-    font-size: 30px;
-    font-style: normal;
-    font-weight: 900;
-    line-height: normal;
-    letter-spacing: 1.5px;
-    color: ${({ theme }) => theme.headerText}; 
-    border-bottom: 1px;
-    border-color: ${({ theme }) => theme.skillsBorderBottom};
+  font-size: 30px;
+  font-style: normal;
+  font-weight: 900;
+  line-height: normal;
+  letter-spacing: 1.5px;
+  color: ${({ theme }) => theme.headerText};
+  border-bottom: 1px;
+  border-color: ${({ theme }) => theme.skillsBorderBottom};
 `;
