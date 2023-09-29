53bd889478f18a6268abbe42861fda0068c4bd82
Wojciech Migdał <122496250+WoytekMig@users.noreply.github.com>
2023-09-29 10:44:08 +0200

    added styledHeader and styledContent to theme

--- src/components/Skills/styled.js
+++ src/components/Skills/styled.js
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+import { StyledContent, StyledHeader } from "../../themes";
 
 export const Wrapper = styled.div`
   max-width: 1216px;
@@ -6,13 +7,20 @@ export const Wrapper = styled.div`
   padding: 32px;
 `;
 
-export const Header = styled.header`
-  font-size: 30px;
-  font-style: normal;
-  font-weight: 900;
-  line-height: normal;
-  letter-spacing: 1.5px;
-  color: ${({ theme }) => theme.headerText};
-  border-bottom: 1px;
+export const Header = styled(StyledHeader)`
+  border-bottom: 1px solid;
   border-color: ${({ theme }) => theme.skillsBorderBottom};
+  padding-top: 32px;
+  padding-bottom: 15px;
+`;
+
+export const ItemsList = styled(StyledContent)`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  color: ${({ theme }) => theme.primaryText};
+  padding-top: 32px;
+`;
+
+export const Item = styled.li`
+  list-style: circle;
 `;
--- src/themes.js
+++ src/themes.js
@@ -1,49 +1,70 @@
+import styled from "styled-components";
+
 const color = {
-    White: "#FFFFF",
-    WhiteLilac: "#FBFBFE",
-    DodgerBlue: "#2188FF",
-    ScienceBlue: "#0366D6",
-    Mercury: "#E5E5E5",
-    Iron: "#D1D5DA",
-    SlateGrey: "#6E7E91",
-    MineShaft: "#252525",
-    Black: "black",
+  White: "#FFFFFF",
+  WhiteLilac: "#FBFBFE",
+  DodgerBlue: "#2188FF",
+  ScienceBlue: "#0366D6",
+  Mercury: "#E5E5E5",
+  Iron: "#D1D5DA",
+  IronLight: "rgba(209, 213, 218, 0.30)",
+  SlateGrey: "#6E7E91",
+  SlateGreyLight: "rgba(54, 54, 54, 0.72)",
+  MineShaft: "#252525",
+  Black: "black",
 };
 
 const common = {
-    breakpoint: {
-        mobileMax: 767,
-        tabletVerticalMax: 990,
-        tabletHorizontalMax: 1200,
-    },
-    boxShadow: "0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03)",
-    borderRadiusSmall: "4px",
+  breakpoint: {
+    mobileMax: 767,
+    tabletVerticalMax: 990,
+    tabletHorizontalMax: 1200,
+  },
+  boxShadow:
+    "0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03)",
+  borderRadiusSmall: "4px",
 };
 
 export const light = {
-    ...common,
+  ...common,
 
-    primaryBackground: color.WhiteLilac,
-    primaryText: color.SlateGrey,
-    headerText: color.MineShaft,
-    hireMeButton: color.ScienceBlue,
-    themeToggleBackground: color.Iron,
-    themeGraphicBackground: color.SlateGrey,
-    themeGraphic: color.WhiteLilac,
-    skillsBorderBottom: color.WhiteLilac,
-    skillsBackground: color.White,
+  primaryBackground: color.WhiteLilac,
+  headerText: color.MineShaft,
+  primaryText: color.SlateGrey,
+  hireMeButton: color.ScienceBlue,
+  themeToggleBackground: color.Iron,
+  themeGraphicBackground: color.SlateGrey,
+  themeGraphic: color.WhiteLilac,
+  skillsBorderBottom: color.IronLight,
+  skillsBackground: color.White,
 };
 
 export const dark = {
-    ...common,
+  ...common,
 
-    primaryBackground: color.MineShaft,
-    primaryText: color.WhiteLilac,
-    headerText: color.WhiteLilac,
-    hireMeButton: color.DodgerBlue,
-    themeToggleBackground: color.MineShaft,
-    themeGraphicBackground: color.WhiteLilac,
-    themeGraphic: color.Black,
-    skillsBorderBottom: color.Iron,
-    skillsBackground: color.SlateGrey,
+  primaryBackground: color.MineShaft,
+  primaryText: color.WhiteLilac,
+  headerText: color.WhiteLilac,
+  hireMeButton: color.DodgerBlue,
+  themeToggleBackground: color.MineShaft,
+  themeGraphicBackground: color.WhiteLilac,
+  themeGraphic: color.Black,
+  skillsBorderBottom: color.Iron,
+  skillsBackground: color.SlateGreyLight,
 };
+
+export const StyledHeader = styled.header`
+  font-size: 30px;
+  font-weight: 900;
+  line-height: normal;
+  letter-spacing: 1.5px;
+  color: ${({ theme }) => theme.headerText};
+`;
+
+export const StyledContent = styled.article`
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 140%;
+  letter-spacing: 0.9;
+  color: ${({ theme }) => theme.primaryText};
+`;
