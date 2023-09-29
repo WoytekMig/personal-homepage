abed59aed93d1b5dcd6a7a83fe6e4d5d33d0077c
Wojciech Migdał <122496250+WoytekMig@users.noreply.github.com>
2023-09-29 17:52:46 +0200

    changed path to icon folder

--- src/components/Header/HireMeButton/styled.js
+++ src/components/Header/HireMeButton/styled.js
@@ -1,29 +1,28 @@
 import { styled } from "styled-components";
-import { ReactComponent as HireMeIcon } from "../icon/Message.svg";
-
+import { ReactComponent as HireMeIcon } from "../../icon/Message.svg";
 
 export const ButtonLink = styled.a`
-    text-decoration: none;
+  text-decoration: none;
 `;
 
 export const StyledButton = styled.button`
-    display: inline-flex;
-    padding: 12px 16px;
-    justify-content: center;
-    align-items: center;
-    gap: 16px;
-    border-radius: 4px;
-    border: 1px solid  rgba(209, 213, 218, 0.30);
-    font-family: inherit;
-    font-style: normal;
-    font-weight: 600;
-    line-height: normal;
-    color: white;
-    background-color: ${({ theme }) => theme.hireMeButton};
+  display: inline-flex;
+  padding: 12px 16px;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+  border-radius: 4px;
+  border: 1px solid rgba(209, 213, 218, 0.3);
+  font-family: inherit;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  color: white;
+  background-color: ${({ theme }) => theme.blueElements};
 `;
 
 export const Icon = styled(HireMeIcon)`
-    width: 24px;
-    height: 24px;
-    background-color: ${({ theme }) => theme.hireMeButton};
+  width: 24px;
+  height: 24px;
+  background-color: ${({ theme }) => theme.blueElements};
 `;
