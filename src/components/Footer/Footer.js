a3449c29457d002a5df56e55b36e825ef249fed9
Wojciech Migdał <122496250+WoytekMig@users.noreply.github.com>
2023-11-03 15:08:19 +0100

    added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,45 @@
+import {
+  Content,
+  EmailAddress,
+  Facebook,
+  FooterWrapper,
+  GitHub,
+  Header,
+  LinkedIN,
+  LogoBox,
+  LogosContainer,
+} from "./styled";
+
+const Footer = () => (
+  <>
+    <FooterWrapper>
+      <Header>Let's talk !</Header>
+      <EmailAddress
+        href="mailto:wojciechjanmigdal@gmail.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        wojciechjanmigdal@gmail.com
+      </EmailAddress>
+      <Content>
+        I'm open to new opportunities 🧐 If you need a task-focused, very
+        experienced in teamwork and teamleading person - feel free to contact me
+        😀
+      </Content>
+
+      <LogosContainer>
+        <LogoBox>
+          <GitHub />
+        </LogoBox>
+        <LogoBox>
+          <LinkedIN />
+        </LogoBox>
+        <LogoBox>
+          <Facebook />
+        </LogoBox>
+      </LogosContainer>
+    </FooterWrapper>
+  </>
+);
+
+export default Footer;
